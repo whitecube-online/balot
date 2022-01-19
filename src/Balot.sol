8d5f736055b5b37d79e3e3f1289e1985a2dd1384
Tim Daubenschütz <tim@daubenschuetz.de>
2022-01-19 18:27:32 +0100

    Implement `safeMint`

--- src/Balot.sol
+++ src/Balot.sol
@@ -1,9 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-import "openzeppelin-contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "openzeppelin-contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "openzeppelin-contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
+import {Counters} from "openzeppelin-contracts/utils/Counters.sol";
 
-contract Balot is ERC721 {
-  constructor() ERC721("Balot", "BALOT") {
+contract Balot is ERC721URIStorage, Ownable {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721("Balot", "BALOT") {}
+
+  function safeMint(
+    address to,
+    string memory tokenURI
+  ) external onlyOwner returns (uint256) {
+    uint256 newTokenId = _tokenIds.current();
+    _safeMint(to, newTokenId);
+    _setTokenURI(newTokenId, tokenURI);
+
+    _tokenIds.increment();
+    return newTokenId;
   }
 }
--- src/Balot.t.sol
+++ src/Balot.t.sol
@@ -5,14 +5,50 @@ import "ds-test/test.sol";
 
 import {Balot} from "./Balot.sol";
 
+contract MockCaller {
+  function callFrom(address token) public {
+      Balot balot = Balot(token);
+      string memory uri0 = "https://unauthorized.com";
+      balot.safeMint(msg.sender, uri0);
+  }
+}
+
 contract BalotTest is DSTest {
     Balot balot;
+    MockCaller mc;
 
     function setUp() public {
         balot = new Balot();
+        mc = new MockCaller();
     }
 
-    function testGetName() public {
+    function testCollectionData() public {
       assertEq(balot.name(), "Balot");
+      assertEq(balot.symbol(), "BALOT");
+    }
+
+    function testFailMintingFromUnauthorizedAddress() public {
+      // NOTE: We want to make sure that mc.callFrom fails
+      mc.callFrom(address(balot));
+
+      // NOTE: We also want to make sure that no token was minted
+      string memory uri0 = "https://example.com/metadata0.json";
+      uint256 tokenId0 = balot.safeMint(msg.sender, uri0);
+      assertEq(tokenId0, 0);
+    }
+
+    function testSafeMint() public {
+      string memory uri0 = "https://example.com/metadata0.json";
+      uint256 tokenId0 = balot.safeMint(msg.sender, uri0);
+      assertEq(tokenId0, 0);
+      assertEq(balot.tokenURI(tokenId0), uri0);
+      assertEq(balot.tokenURI(tokenId0), uri0);
+      assertEq(balot.ownerOf(tokenId0), msg.sender);
+
+      string memory uri1 = "https://example.com/metadata1.json";
+      uint256 tokenId1 = balot.safeMint(msg.sender, uri1);
+      assertEq(tokenId1, 1);
+      assertEq(balot.tokenURI(tokenId1), uri1);
+      assertEq(balot.ownerOf(tokenId1), msg.sender);
     }
 }
