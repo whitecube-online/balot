739c76c6e0f339d8b01d70863e74965f709b12fd
Tim Daubenschütz <tim@daubenschuetz.de>
2022-05-27 10:57:50 +0200

    Include .editorconfig to normalize 2 spaces
    - Fixes #21

--- src/Minter.sol
+++ src/Minter.sol
@@ -9,21 +9,21 @@ interface Balot {
 }
 
 contract Minter {
-	function safeMintRange(
-		address collection,
-		address nextOwner,
-		address to,
-		uint16 start,
-		uint16 end,
-		uint16 step
-	) external {
-		Balot b = Balot(collection);
-		for (uint256 tokenId = start; tokenId <= end; tokenId += step) {
-			b.safeMint(to, string(abi.encodePacked(
-				Strings.toString(tokenId),
-				".json"
-			)));
-		}
-		b.transferOwnership(nextOwner);
-	}
+  function safeMintRange(
+    address collection,
+    address nextOwner,
+    address to,
+    uint16 start,
+    uint16 end,
+    uint16 step
+  ) external {
+    Balot b = Balot(collection);
+    for (uint256 tokenId = start; tokenId <= end; tokenId += step) {
+      b.safeMint(to, string(abi.encodePacked(
+        Strings.toString(tokenId),
+        ".json"
+      )));
+    }
+    b.transferOwnership(nextOwner);
+  }
 }
